e9751db0c07cea23bee085c73f6f35ccfcee8a62
ismail.nyzar <ismail.nyzar@pickme.lk>
2025-05-11 19:33:43 +0530

    fresh start

--- src/main/java/W2151443/concertticketsystemv2/infrastructure/coordination/zk/ZooKeeperConnectionListener.java
+++
@@ -1,7 +0,0 @@
-package W2151443.concertticketsystemv2.infrastructure.coordination.zk;
-
-public interface ZooKeeperConnectionListener {
-    void onZooKeeperConnected();
-    void onZooKeeperDisconnected();
-    void onZooKeeperSessionExpired();
-}
